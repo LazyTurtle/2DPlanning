532a69e2a8ca1a833fa837488bc1071eb7058a80
Jhonny Hueller <jh.hueller@gmail.com>
2022-12-13 17:27:09 +0100

    minor change

--- src/dubins_planner/src/multi_point_dubins_calculator.cpp
+++ src/dubins_planner/src/multi_point_dubins_calculator.cpp
@@ -20,14 +20,12 @@ class MultiPointDubinsCalculator : public rclcpp::Node
       dubins_calculator = this->create_client<dubins_planner_msgs::srv::DubinsPlanning>("dubins_calculator");
       log_info("Waiting for dubins calculator...");
 
-      while (!dubins_calculator->wait_for_service(2s)) {
+      while (!dubins_calculator->wait_for_service(3s)) {
         if (!rclcpp::ok()) {
           log_err("Interrupted while waiting for the service. Exiting.");
         }
         log_info("Service not available, waiting again...");
       }
-
-      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Dubins calculator available.");
 
       service = this->create_service<dubins_planner_msgs::srv::MultiPointDubinsPlanning>
         ("multi_point_dubins_calculator", 
@@ -39,7 +37,7 @@ class MultiPointDubinsCalculator : public rclcpp::Node
 
       log_info("Multi point dubins calculator ready.");
     }
-
+  
     void calculate(
       const std::shared_ptr<dubins_planner_msgs::srv::MultiPointDubinsPlanning::Request> request,
       const std::shared_ptr<dubins_planner_msgs::srv::MultiPointDubinsPlanning::Response> response){
